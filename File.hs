ef2a3071205739b2686a5c597c5a080cac288b98
Bercovici Adrian <bercovici.adrian@outlook.com>
2018-09-13 19:12:09 +0300

    end of day comm

--- File.hs
+++ File.hs
@@ -1,17 +1,20 @@
 module File(parseFile) where 
     import qualified Data.Text as T
     import qualified Data.Text.IO as I
+    import Text.Read
+    import Data.List
     import Control.Monad(forM,mapM,forM_)
     import Prelude hiding((<>))
 
 
+    coma=((<>) ",") 
+
     (<>)::String->T.Text
     (<>) = T.pack
-    
     (><)::T.Text->String
     (><) =T.unpack
 
-    data File=Rfile (Maybe Readme) | Dfile ([Maybe Double]) deriving(Show)
+    data File=Rfile (Maybe Readme) | Dfile ([Maybe Double]) 
     
     data Readme=Readme{
         maxClients::Int,
@@ -22,9 +25,39 @@ module File(parseFile) where
         stepDelay::Int
     }deriving(Show)
     
-    
-
+    instance Show File where 
+        show =showFile
+            --Data.List.intercalate "," [show e|e@(T (Just e)) <- ls]
+    showFile::File->String
+    showFile (Rfile Nothing) ="empty readme file"
+    showFile (Rfile (Just x))=show x
+    showFile (Dfile []) ="empty data file"
+    showFile (Dfile ls)=Data.List.intercalate "," [show e| Just e<-ls] 
     --first line split in 6 elements
+
+    parseFile::FilePath->IO (Maybe File)
+    parseFile fileName=I.readFile fileName>>= \text ->
+        do
+        let ! lines =  T.splitOn  coma text
+        let len=Prelude.length lines
+        case len of 
+         1 -> do
+            let rowCells=T.splitOn coma (head lines)
+            let readme=parseRfile rowCells
+            case Prelude.length rowCells of
+                 6 -> return (Just (Rfile readme))
+                 _ -> error "invalid format"
+
+         _ -> do
+                I.putStrLn . (<>) $"reached other branch"
+                content <- parseDfile lines
+               
+                forM_ content $ \mline ->
+                    case mline of
+                        Nothing -> I.putStrLn ((<>)"INVALID line")
+                        Just val->I.putStrLn ((<>) .show $ val)
+                return (Just (Dfile  content))
+    
     parseRfile::[T.Text]->Maybe Readme
     parseRfile arr |  Prelude.length arr /= 6  = Nothing
                    | otherwise = Just Readme{
@@ -37,33 +70,22 @@ module File(parseFile) where
                      }
     --data lines
     parseDfile::[T.Text]->IO [Maybe Double]
-    parseDfile lines = forM lines $ \line ->
-                          do   
-                          let value = (read::String->Double) . T.unpack $ line 
-                          print .show $ value
-                          return (Just value)
+    parseDfile []= return [Nothing]
+    parseDfile lines =return [e|Just e<-lines]
+        
+                          
         
               
     
-   
     
-    parseFile::FilePath->IO (Maybe File)
-    parseFile fileName=I.readFile fileName>>= \text ->
-        do
-        let ! lines =  T.splitOn  ("," <>)  text
-        let len=Prelude.length lines
-        if len==1 then
-            do
-                let content=head  lines
-                let values=T.splitOn ("," <>) content   -- de ce nu merge ( "," <>) ca la (1+)
-                let readme=return Readme (parseRfile values)
-                case Prelude.length content of
-                     6 -> return (Just (Rfile readme))
-                     _ -> error "invalid format"
-        else
-            do
-                content <- parseDfile lines
-                return (Just Dfile  content)
+    
+    
+
+    main::IO()
+    main= do
+        dt<-parseFile "test.txt"
+        Prelude.putStrLn "finished"
+        I.writeFile "out.txt" (T.pack.show $dt)
                 
         
     
--- main.hs
+++ main.hs
@@ -3,46 +3,23 @@ module Main where
     import  Data.Text 
     import Data.List  as L
     import qualified Data.Text.IO as I
-    
+    import File
     import Control.Monad(forM,mapM,filterM)
 
-    data Folder=Folder{
-        name::String,
-        files::[File]
-    }deriving(Show)
-    instance Show File where
-        show (Rfile readme)=Prelude.map show 
-                                     [maxClients readme,
-                                      minClients readme,
-                                      stepClients readme,
-                                      maxDelay readme,
-                                      minDelay readme,
-                                      stepDelay readme]
-    instance Show Folder where
-        show folder="{ folderName:"++ (name folder)+"\r\nData:\r\n"
-                                   ++ (L.intercalate  "," (Prelude.map show files) ) 
+    
     
 
-    dataPath="Data\\"
-    outPath="out"
+  
 
-    main::IO [Folder]
+    main::IO()
     main=do 
-        folderNames<-filterM doesDirectoryExist (listDirectory dataPath)
-        let folders=Prelude.map parseFolder folderNames
+        dat<-parseFile "test.txt"
+        putStrLn $ show dat
+        --folderNames<-filterM doesDirectoryExist (listDirectory dataPath)
+        --let folders=Prelude.map parseFolder folderNames
 
 
         
         
     
-    parseFolder::Text->IO Folder
-    makeFolder folderPath=do
-        fileNames<-filterM doesDirectoryExist 
-    
-    
-    
-
-    makeFile::Text->IO File
-    makeFile fileName=do
-        contents<-readFile fileName>>= splitOn "\r\n"  
-        return parseFile contents 
+   
