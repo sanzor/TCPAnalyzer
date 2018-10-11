9af7249c8ab9168d25b882626f76e9f9877c1308
Bercovici Adrian <bercovici.adrian@outlook.com>
2018-10-11 17:40:35 +0300

    end of day commit

--- File.hs
+++ File.hs
@@ -3,7 +3,8 @@ module File where
     import Data.Text(splitOn,Text,concat,pack,unpack,head,tail,take) 
     import Data.Maybe(Maybe,mapMaybe,fromMaybe)
     import Data.List(intercalate) 
-    import Prelude hiding(take,tail,head)
+   
+
 
     data Readme=Readme{
         maxClients::Int,
@@ -28,48 +29,48 @@ module File where
         toText::a->Text
         fromText::Text->a
 
+
     instance TextEncode Samples where
         toText s=Data.Text.concat ( mapMaybe select (values s)) where
-             select mDouble=case mDouble of 
-                Just value ->Just (pack.show $ value)
-                Nothing -> Nothing
+                 select mDouble=case mDouble of 
+                    Just value ->Just (pack.show $ value)
+                    Nothing -> Nothing
         fromText text=Samples  (map (readMaybe.unpack) (splitOn (pack ",") text))
-    
-   
-        
+
     instance TextEncode Readme where
-            toText r=pack $ intercalate ","  (map  show [maxClients r,minClients r,stepClients r,maxDelay  r,minDelay r,stepDelay r])
-            fromText = Data.Text.take 6 . readContent $ 
-    
+         toText r=pack $ intercalate ","  (map  show [maxClients r,minClients r,stepClients r,maxDelay  r,minDelay r,stepDelay r])
+         fromText txt = Readme{
+                        maxClients=Prelude.head dat,
+                        minClients=dat!!1,
+                        stepClients=dat!!2,
+                        maxDelay=dat!!3,
+                        minDelay=dat!!4,
+                        stepDelay=dat!!5} where
+                            dat=Prelude.take 6 . readData $ txt
+
+
+
     instance TextEncode File where
-            toText f=case f of 
-               Rfile mr -> pack ("{"++ (unpack . toText $ mr) ++ "}") 
-               Dfile s  -> pack . toText $ s
-            fromText txt =readFileData (FileData (Data.Text.head txt) (Data.Text.tail txt))
-                 
-    
+        toText f=case f of 
+               Rfile mr -> pack ("{"++ unpack (toText $ fromMaybe (Readme 0 0 1 2 3 2)  mr)++"}")
+               Dfile s  -> toText  s
+               Empty ->pack "Empty File"
+        fromText txt =readFileData (FileData (Data.Text.head txt) (Data.Text.tail txt))   
 
     readFileData::FileData->File
-    readHeader fd=case header fd of
-        'r'->take 6 . readData . rawContent $ fd
-        'd'->readData . rawContent $ fd
-        _  ->Empty
-                
-    readData::Text->[Double]
+    readFileData fd=case header fd of
+            'r'->Data.Text.take 6 . readData . rawContent $ fd
+            'd'->readData . rawContent $ fd
+            _  ->Empty
+                    
+    readData::Text->[Int]
     readData =catMaybes . readDataRaw .unpack 
-
-    readDataRaw::String->Maybe [Double]
+    
+    readDataRaw::String->Maybe [Int]
     readDataRaw (x:xs)=go [] [](x:xs) where
-        go _ ls []         =if length length ls > 0  then Just ls else Nothing
-        go small big (x:xs)= case x of
-                              '}' -> Just (small:big) 
-                              ',' -> go [] small:big  xs 
-                               
-                              
-                              
-                             
-                              
-
-
+            go _ ls []         =if length length ls > 0  then Just ls else Nothing
+            go small big (x:xs)= case x of
+                                  '}' -> Just (small:big) 
+                                  ',' -> go [] small:big  xs 
     instance Show File where
-        show =unpack.fromText 
+            show =unpack.fromText              
--- err.hs
+++ err.hs
@@ -1,112 +1,57 @@
 
-File.hs:36:28: error:
-    * Couldn't match expected type `Text' with actual type `[a2]'
-    * In the expression:
-        (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
+File.hs:62:18: error:
+    * Couldn't match expected type `File' with actual type `Text'
+    * In the expression: Data.Text.take 6 . readData . rawContent $ fd
       In a case alternative:
-          (Rfile mr)
-            -> (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
+          'r' -> Data.Text.take 6 . readData . rawContent $ fd
       In the expression:
-        case f of
-          (Rfile mr)
-            -> (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
-          (Dfile s) -> undefined
+        case header fd of
+          'r' -> Data.Text.take 6 . readData . rawContent $ fd
+          'd' -> readData . rawContent $ fd
+          _ -> Empty
    |
-36 |              (Rfile mr) -> (pack "r:{")++fromMaybe (pack "emptyFile") mr++(pack "r:}")
-   |                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+62 |             'r'->Data.Text.take 6 . readData . rawContent $ fd
+   |                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-File.hs:36:29: error:
-    * Couldn't match expected type `[a2]' with actual type `Text'
-    * In the first argument of `(++)', namely `(pack "r:{")'
+File.hs:62:37: error:
+    * Couldn't match type `[Int]' with `Text'
+      Expected type: FileData -> Text
+        Actual type: FileData -> [Int]
+    * In the second argument of `(.)', namely `readData . rawContent'
+      In the expression: Data.Text.take 6 . readData . rawContent
+      In the expression: Data.Text.take 6 . readData . rawContent $ fd
+   |
+62 |             'r'->Data.Text.take 6 . readData . rawContent $ fd
+   |                                     ^^^^^^^^^^^^^^^^^^^^^
+
+File.hs:63:18: error:
+    * Couldn't match expected type `File' with actual type `[Int]'
+    * In the expression: readData . rawContent $ fd
+      In a case alternative: 'd' -> readData . rawContent $ fd
       In the expression:
-        (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
-      In a case alternative:
-          (Rfile mr)
-            -> (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
+        case header fd of
+          'r' -> Data.Text.take 6 . readData . rawContent $ fd
+          'd' -> readData . rawContent $ fd
+          _ -> Empty
    |
-36 |              (Rfile mr) -> (pack "r:{")++fromMaybe (pack "emptyFile") mr++(pack "r:}")
-   |                             ^^^^^^^^^^
+63 |             'd'->readData . rawContent $ fd
+   |                  ^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-File.hs:36:42: error:
-    * Couldn't match expected type `[a2]' with actual type `Text'
-    * In the first argument of `(++)', namely
-        `fromMaybe (pack "emptyFile") mr'
-      In the second argument of `(++)', namely
-        `fromMaybe (pack "emptyFile") mr ++ (pack "r:}")'
-      In the expression:
-        (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
+File.hs:67:15: error:
+    * Variable not in scope: catMaybes :: Maybe [Int] -> [Int]
+    * Perhaps you want to add `catMaybes' to the import list
+      in the import of `Data.Maybe' (File.hs:4:5-47).
    |
-36 |              (Rfile mr) -> (pack "r:{")++fromMaybe (pack "emptyFile") mr++(pack "r:}")
-   |                                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+67 |     readData =catMaybes . readDataRaw .unpack 
+   |               ^^^^^^^^^
 
-File.hs:36:71: error:
-    * Couldn't match type `Readme' with `Text'
-      Expected type: Maybe Text
-        Actual type: Maybe Readme
-    * In the second argument of `fromMaybe', namely `mr'
-      In the first argument of `(++)', namely
-        `fromMaybe (pack "emptyFile") mr'
-      In the second argument of `(++)', namely
-        `fromMaybe (pack "emptyFile") mr ++ (pack "r:}")'
-   |
-36 |              (Rfile mr) -> (pack "r:{")++fromMaybe (pack "emptyFile") mr++(pack "r:}")
-   |                                                                       ^^
-
-File.hs:36:76: error:
-    * Couldn't match expected type `[a2]' with actual type `Text'
-    * In the second argument of `(++)', namely `(pack "r:}")'
-      In the second argument of `(++)', namely
-        `fromMaybe (pack "emptyFile") mr ++ (pack "r:}")'
-      In the expression:
-        (pack "r:{") ++ fromMaybe (pack "emptyFile") mr ++ (pack "r:}")
-   |
-36 |              (Rfile mr) -> (pack "r:{")++fromMaybe (pack "emptyFile") mr++(pack "r:}")
-   |                                                                            ^^^^^^^^^^
-
-File.hs:38:23: error:
-    * Couldn't match expected type `Text' with actual type `[Text]'
-    * In the pattern: x : xs
-      In an equation for `fromText':
-          fromText (x : xs)
-            = case Data.Text.head x of
-                'r' -> readContent xs
-                'd' -> undefined
-      In the instance declaration for `TextEncode File'
-   |
-38 |             fromText (x:xs)= case Data.Text.head  x of
-   |                       ^^^^
-
-File.hs:39:20: error:
-    * Couldn't match expected type `File'
-                  with actual type `Maybe [Double]'
-    * In the expression: readContent xs
-      In a case alternative: 'r' -> readContent xs
-      In the expression:
-        case Data.Text.head x of
-          'r' -> readContent xs
-          'd' -> undefined
-   |
-39 |              'r' ->readContent xs
-   |                    ^^^^^^^^^^^^^^
-
-File.hs:39:32: error:
-    * Couldn't match type `Text' with `Char'
-      Expected type: String
-        Actual type: [Text]
-    * In the first argument of `readContent', namely `xs'
-      In the expression: readContent xs
-      In a case alternative: 'r' -> readContent xs
-   |
-39 |              'r' ->readContent xs
-   |                                ^^
-
-File.hs:43:24: error:
-    * Couldn't match type `[a0]' with `Double'
-      Expected type: Maybe [Double]
+File.hs:70:24: error:
+    * Couldn't match type `[a0]' with `Int'
+      Expected type: Maybe [Int]
         Actual type: Maybe [[a0]]
     * In the expression: go [] [] (x : xs)
-      In an equation for `readContent':
-          readContent (x : xs)
+      In an equation for `readDataRaw':
+          readDataRaw (x : xs)
             = go [] [] (x : xs)
             where
                 go _ ls [] = if length length ls > 0 then Just ls else Nothing
@@ -115,10 +60,10 @@ File.hs:43:24: error:
                       '}' -> Just (small : big)
                       ',' -> go ... small : big xs
    |
-43 |     readContent (x:xs)=go [] [](x:xs) where
+70 |     readDataRaw (x:xs)=go [] [](x:xs) where
    |                        ^^^^^^^^^^^^^^
 
-File.hs:44:32: error:
+File.hs:71:36: error:
     * Couldn't match expected type `[[a]] -> Integer'
                   with actual type `Int'
     * The function `length' is applied to two arguments,
@@ -126,13 +71,13 @@ File.hs:44:32: error:
       In the first argument of `(>)', namely `length length ls'
       In the expression: length length ls > 0
     * Relevant bindings include
-        ls :: [[a]] (bound at File.hs:44:14)
-        go :: [a] -> [a] -> [Char] -> Maybe [[a]] (bound at File.hs:44:9)
+        ls :: [[a]] (bound at File.hs:71:18)
+        go :: [a] -> [a] -> [Char] -> Maybe [[a]] (bound at File.hs:71:13)
    |
-44 |         go _ ls []         =if length length ls > 0  then Just ls else Nothing
-   |                                ^^^^^^^^^^^^^^^^
+71 |             go _ ls []         =if length length ls > 0  then Just ls else Nothing
+   |                                    ^^^^^^^^^^^^^^^^
 
-File.hs:47:38: error:
+File.hs:74:42: error:
     * Couldn't match expected type `Maybe [[a]]'
                   with actual type `[[Char] -> Maybe [[a]]]'
     * In the expression: go [] small : big xs
@@ -142,14 +87,14 @@ File.hs:47:38: error:
           '}' -> Just (small : big)
           ',' -> go [] small : big xs
     * Relevant bindings include
-        big :: [[a]] (bound at File.hs:45:18)
-        small :: [a] (bound at File.hs:45:12)
-        go :: [a] -> [a] -> [Char] -> Maybe [[a]] (bound at File.hs:44:9)
+        big :: [[a]] (bound at File.hs:72:22)
+        small :: [a] (bound at File.hs:72:16)
+        go :: [a] -> [a] -> [Char] -> Maybe [[a]] (bound at File.hs:71:13)
    |
-47 |                               ',' -> go [] small:big  xs 
-   |                                      ^^^^^^^^^^^^^^^^^^^
+74 |                                   ',' -> go [] small:big  xs 
+   |                                          ^^^^^^^^^^^^^^^^^^^
 
-File.hs:47:50: error:
+File.hs:74:54: error:
     * Couldn't match expected type `[Char] -> [[Char] -> Maybe [[a]]]'
                   with actual type `[[a]]'
     * The function `big' is applied to one argument,
@@ -157,14 +102,14 @@ File.hs:47:50: error:
       In the second argument of `(:)', namely `big xs'
       In the expression: go [] small : big xs
     * Relevant bindings include
-        big :: [[a]] (bound at File.hs:45:18)
-        small :: [a] (bound at File.hs:45:12)
-        go :: [a] -> [a] -> [Char] -> Maybe [[a]] (bound at File.hs:44:9)
+        big :: [[a]] (bound at File.hs:72:22)
+        small :: [a] (bound at File.hs:72:16)
+        go :: [a] -> [a] -> [Char] -> Maybe [[a]] (bound at File.hs:71:13)
    |
-47 |                               ',' -> go [] small:big  xs 
-   |                                                  ^^^^^^^
+74 |                                   ',' -> go [] small:big  xs 
+   |                                                      ^^^^^^^
 
-File.hs:56:15: error:
+File.hs:76:19: error:
     * Couldn't match type `File' with `Text'
       Expected type: File -> String
         Actual type: Text -> String
@@ -172,5 +117,5 @@ File.hs:56:15: error:
       In an equation for `show': show = unpack . fromText
       In the instance declaration for `Show File'
    |
-56 |         show =unpack.fromText 
-   |               ^^^^^^^^^^^^^^^
+76 |             show =unpack.fromText              
+   |                   ^^^^^^^^^^^^^^^
