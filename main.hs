ea172c9afebf8b48aa5b01e15d2fb975a033f2f9
Bercovici Adrian <bercovici.adrian@outlook.com>
2019-01-04 16:56:30 +0200

    Started parsing folder

--- main.hs
+++ main.hs
@@ -1,24 +1,46 @@
 module Main where 
     import Prelude hiding(readFile,writeFile)
-    import Data.Text(Text,pack,unpack)
     import Data.Text.IO(readFile,writeFile)
-    import System.Directory(getDirectoryContents)
-    import Classes(TextEncode,toText,fromText)
-    import Control.Monad(forM_,filterM)
-    import Text.Regex
+    import Data.List(intercalate)
+    import Data.Text(Text)
+    import System.Directory(getDirectoryContents,getCurrentDirectory)
     import qualified Data.Map as Map
+    import Control.Monad(forM_,forM)
+    import System.Environment(getArgs)
+
+
     import Tcp(TCPFile)
+    import Classes(TextEncode,
+                   toText,
+                   fromText)
+    import Utils(u,p)
+    
+
     main::IO()
     main=do
-        putStrLn "aa"
+        let fpath="files"
+        fileNames<- fmap filterFiles $  getFiles fpath
+        fn<-mapM (\x ->
+            do 
+               let p= pathCombine [fpath,x]
+               fmap (writeFile ("./out/"++x)) (readFile p)
+               
+               
+            ) fileNames
+        putStrLn "done"
 
+    parse::Text->TCPFile
+    parse =fromText       
+    pathCombine::[String]->String
+    pathCombine ls="." ++ foldl (\x y-> x++"/"++y) "" ls
         
     getFiles::FilePath->IO [String]
     getFiles  = getDirectoryContents
 
+    filterFiles::[String]->[String]
+    filterFiles = filter (\x-> head x /= '.')
+
    
-
-
    
 
    
